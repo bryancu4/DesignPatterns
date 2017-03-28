947dc4f9ce2f8f8fb701c4f856f396190ef6a412
Bryan Cua <bryancu4@gmail.com>
2017-03-28 23:31:39 +0100

    BuilderPattern - Problem 3

---
+++ BuilderPattern/MySandwichBuilder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BuilderPattern
+{
+    public class MySandwichBuilder
+    {
+        private Sandwich sandwich;
+
+        public Sandwich GetSandwich()
+        {
+            return sandwich;
+        }
+
+        public void CreateSandwich()
+        {
+            sandwich = new Sandwich();
+
+            PrepareBread();
+            ApplyMeatAndCheese();
+            ApplyVegetables();
+            AddCondiments();
+        }
+
+        private void AddCondiments()
+        {
+            sandwich.HasMayo = false;
+            sandwich.HasMustard = true;
+        }
+
+        private void ApplyVegetables()
+        {
+            sandwich.Vegetables = new List<string> { "Tomato", "Onion" };
+        }
+
+        private void ApplyMeatAndCheese()
+        {
+            sandwich.CheeseType = CheeseType.Cheddar;
+            sandwich.MeatType = MeatType.Turkey;
+        }
+
+        private void PrepareBread()
+        {
+            sandwich.BreadType = BreadType.White;
+            sandwich.IsToasted = true;
+        }
+    }
+}
--- BuilderPattern/Program.cs
+++ BuilderPattern/Program.cs
@@ -7,13 +7,10 @@ namespace BuilderPattern
     {
         static void Main(string[] args)
         {
-            var sandwich = new Sandwich();
-            sandwich.BreadType = BreadType.White;
-            sandwich.CheeseType = CheeseType.Cheddar;
-            sandwich.HasMayo = false;
-            sandwich.IsToasted = true;
-            sandwich.HasMustard = true;
-            sandwich.Vegetables = new List<string> { "Tomato", "Onion" };
+            var builder = new MySandwichBuilder();
+
+            builder.CreateSandwich();
+            var sandwich = builder.GetSandwich();
             sandwich.Display();
 
             Console.ReadKey();
